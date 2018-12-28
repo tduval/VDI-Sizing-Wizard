d6436bdb0550c0dc1178d1b8921fee5d55cba4db
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-12-28 17:11:18 +0800

    Install vue-units filters

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import VueUnits from 'vue-units'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
+Vue.use(VueUnits)
 
 new Vue({
   router,
