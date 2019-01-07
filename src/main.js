5951d0bc2eec52cfff4fc5dbd3becb6fc48286e9
Thomas Duval <Thomas.Duval@emeriocorp.com>
2019-01-07 16:25:24 +0800

    Remove all prerendering stuff

--- src/main.js
+++ src/main.js
@@ -10,12 +10,8 @@ Vue.config.productionTip = false
 Vue.use(Vuetify)
 Vue.use(VueUnits)
 
-const root = new Vue({
+new Vue({
   router,
   store,
   render: h => h(App)
-})
-
-document.addEventListener('DOMContentLoaded', function () {
-  root.$mount('#app')
-})
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/VDI-Sizing-Wizard/'
-    : '/',
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        routes: [ '/' ]
-      })
-    ]
-  }
+    : '/'
 }
