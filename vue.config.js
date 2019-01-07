8ad0dfafe40d22db3d4c1cf64f877a2b769477fc
Thomas Duval <Thomas.Duval@emeriocorp.com>
2019-01-07 13:52:55 +0800

    remove babel-polyfill but add prerender-spa-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/VDI-Sizing-Wizard/'
-    : '/'
+    : '/',
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: [ '/' ]
+      })
+    ]
+  }
 }
